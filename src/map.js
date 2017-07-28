103f132762cc44b8413e3554e2925d69d46104da
Mike <mike706574@gmail.com>
2017-07-27 20:28:39 -0500

    Talk to database.

--- src/map.js
+++ src/map.js
@@ -4,44 +4,36 @@ function vals(map) {
   return Array.from(map.values());
 }
 
-export function getAnimals(params) {
-  return new Promise((resolve, reject) => {
-    let animals = vals(db);
-    if(params.name) {
-      animals = animals.filter(animal => animal.name.includes(params.name));
-    }
-    resolve({status: 'ok', animals: animals});
-  });
+export async function getAnimals(params) {
+  let animals = vals(db);
+  if(params.name) {
+    animals = animals.filter(animal => animal.name.includes(params.name));
+  }
+  return {status: 'ok', animals: animals};
 }
 
-export function getAnimal(name) {
-  return new Promise((resolve, reject) => {
-    if(!name) {
-      resolve({status: 'badRequest', message: 'No name provided.'});
-    }
-    if(db.has(name)) {
-      resolve({status: 'ok', animal: db.get(name)});
-    }
-    resolve({status: 'notFound'});
-  });
+export async function getAnimal(name) {
+  if(!name) {
+    return {status: 'badRequest', message: 'No name provided.'};
+  }
+  if(db.has(name)) {
+    return {status: 'ok', animal: db.get(name)};
+  }
+  return {status: 'notFound'};
 }
 
-export function addAnimal(animal) {
-  return new Promise((resolve, reject) => {
-    if(!animal){
-      resolve({status: 'badRequest', message: 'No animal provided.'});
-    }
-    if(db.has(animal.name)) {
-      resolve({status: 'alreadyExists', animal: animal});
-    }
-    db.set(animal.name, animal);
-    resolve({status: 'created', animal: animal});
-  });
+export async function addAnimal(animal) {
+  if(!animal){
+    return {status: 'badRequest', message: 'No animal provided.'};
+  }
+  if(db.has(animal.name)) {
+    return {status: 'alreadyExists', animal: animal};
+  }
+  db.set(animal.name, animal);
+  return {status: 'created', animal: animal};
 }
 
-export function clearAnimals() {
-  return new Promise((resolve, reject) => {
-    db.clear();
-    resolve({status: 'cleared'});
-  });
+export async function clearAnimals() {
+  db.clear();
+  return {status: 'cleared'};
 }
